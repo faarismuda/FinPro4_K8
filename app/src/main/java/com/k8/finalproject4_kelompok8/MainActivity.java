173778cf0d54d9f30cb1ad40cce19dfb466e3963
faarismuda <faarismudadwinugraha@gmail.com>
2022-12-06 22:25:57 +0700

    Initial commit

---
+++ app/src/main/java/com/k8/finalproject4_kelompok8/MainActivity.java
@@ -0,0 +1,14 @@
+package com.k8.finalproject4_kelompok8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
